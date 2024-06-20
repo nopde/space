08ade8431d3cf81f22f9ead655791f61dcfe4fd3
nopde <tobillodev@gmail.com>
2024-06-20 15:14:05 -0300

    Tooltip system rework

--- src/js/index.js
+++ src/js/index.js
@@ -271,39 +271,43 @@ searchBar.focus();
 
 search.spaceSearch(spaces);
 
-const tooltip = document.querySelector(".tooltip");
-
 function checkTooltipElements() {
     const tooltipElements = document.querySelectorAll("[tooltip]");
+    const tooltips = document.querySelector(".tooltips");
 
     tooltipElements.forEach(tooltipElement => {
-        tooltipElement.addEventListener("mouseenter", event => {
-            tooltip.innerHTML = tooltipElement.getAttribute("data-tooltip");
-            tooltip.classList.remove("hidden");
+        const tooltip = document.createElement("div");
+        tooltip.classList.add("tooltip");
+        tooltip.classList.add("hidden");
 
+        tooltip.innerHTML = tooltipElement.getAttribute("data-tooltip");
+
+        tooltips.appendChild(tooltip);
+
+        tooltipElement.addEventListener("mouseenter", event => {
             const tooltipRect = tooltip.getBoundingClientRect();
             const tooltipElementRect = tooltipElement.getBoundingClientRect();
             const windowWidth = window.innerWidth;
             const windowHeight = window.innerHeight;
 
-            let x = tooltipElementRect.x;
+            let x = tooltipElementRect.x + Math.round(tooltipElementRect.width) / 2 - Math.round(tooltipRect.width) / 2;
             let y = tooltipElementRect.y + tooltipElementRect.height + 5;
 
-            if (tooltipElementRect.x + tooltipRect.width > windowWidth) {
+            if (x > windowWidth) {
                 x = windowWidth - tooltipRect.width - 5;
             }
 
-            if (tooltipElementRect.bottom + tooltipRect.height > windowHeight) {
+            if (tooltipElementRect.bottom + tooltipRect.height + 5 > windowHeight) {
                 y = windowHeight - tooltipRect.height - 5;
             }
 
             tooltip.style.left = x + "px";
             tooltip.style.top = y + "px";
+
+            tooltip.classList.remove("hidden");
         });
 
-        tooltipElement.addEventListener("mouseleave", event => {
-            tooltip.classList.add("hidden");
-        });
+        tooltipElement.addEventListener("mouseleave", () => { tooltip.classList.add("hidden"); });
     });
 }
 
